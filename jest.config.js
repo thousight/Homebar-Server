40ccb970b8fe38ea6f7157b3c8f0f82f4b5e1aa9
Mark Wen <markwenguojie94@gmail.com>
2019-01-14 12:04:16 -0500

    Added jest ts setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts', 'json'],
+  roots: ['<rootDir>/server.ts', '<rootDir>/src'],
+  testRegex: '(.*.(test|spec)).ts?$',
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+}
