258489ace5411693916bae7e6b0587acef8f36c3
Mark Wen <markwenguojie94@gmail.com>
2019-01-21 15:46:22 -0500

    chore: trying to fix jest on travis

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleFileExtensions: ['js', 'ts', 'json'],
-  roots: ['<rootDir>/server.ts', '<rootDir>/src/'],
+  // roots: ['<rootDir>/server.ts', '<rootDir>/src/'],
   testRegex: '(.*.(test|spec)).ts?$',
   testPathIgnorePatterns: ['/node_modules/', '/build/'],
   transform: {
