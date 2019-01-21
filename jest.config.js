ae0db2f88a531a72cdb83adac280d1b509267924
Mark Wen <markwenguojie94@gmail.com>
2019-01-21 16:43:42 -0500

    feat(travis): get travis and heroku working together

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   moduleFileExtensions: ['js', 'ts', 'json'],
-  // roots: ['<rootDir>/server.ts', '<rootDir>/src/'],
   testRegex: '(.*.(test|spec)).ts?$',
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
   transform: {
